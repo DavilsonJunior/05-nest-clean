b884b87df480504a1ba0f506b59588578aff6236
Davilson Paulino da Cunha Junior <davilson.junior@stitcloud.com>
2023-12-06 01:36:26 -0300

    feat: add zod validation

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  // como esta estendendo a classe PrismaClient, dentro delete do construtor dele
+  // tem a instancia do Prisma client
+  constructor() {
+    super({
+      log: ['warn', 'error'],
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
