4b2dbdc61777e20ae2df3ce089c5f6c8ab54fdde
Davilson Paulino da Cunha Junior <davilson.junior@stitcloud.com>
2023-12-07 01:06:07 -0300

    feat: create user jwt

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
-  await app.listen(3333)
+  const configureService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configureService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 bootstrap()
