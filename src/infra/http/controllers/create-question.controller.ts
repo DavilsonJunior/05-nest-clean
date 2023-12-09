28d2980e3a69e3e8deaf75dd8d7a9ee634e6e36e
Davilson Paulino da Cunha Junior <davilson.junior@stitcloud.com>
2023-12-08 23:48:18 -0300

    feat: add clean architecture in the project

---
+++ src/infra/http/controllers/create-question.controller.ts
@@ -0,0 +1,61 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    const slug = this.slugify(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private removeAccents(inputStr: string): string {
+    return inputStr.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  }
+
+  private slugify(text: string): string {
+    // Remove accents
+    text = this.removeAccents(text)
+
+    // Convert string to lowercase and replace spaces with hyphens
+    text = text
+      .toLowerCase()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[-\s]+/g, '-')
+      .trim()
+
+    return text
+  }
+}
